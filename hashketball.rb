37f88ba37e482ea79af35537901d2392f4ce8c2b
Jack T <jackmt9@gmail.com>
2020-02-26 16:54:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,13 +4,26 @@ game_hash
 team = {:home => 
           {:team_name => "Brooklyn Nets", 
           :colors => ["Black", "White"], 
-          :players => [{}]}, 
+          :players => 
+          [
+            {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1
+            }, 
+            :players => [
+            {:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => 
+              
+            }
+            ]
+            } 
+          ]
+          }, 
         :away => 
           {:team_name => "Charlotte Hornets", 
           :colors => ["Turquoise", "Purple"], 
-          :players => [
-            {:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
-            ]
+          :players => 
+          [
+            {:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => 
+            }
+          ]
           } 
         }
 end
