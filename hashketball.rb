937fa8c66f93732efaa4f9552e8e54a4eec0b4a8
Jack T <jackmt9@gmail.com>
2020-02-26 16:51:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,11 +4,15 @@ game_hash
 team = {:home => 
           {:team_name => "Brooklyn Nets", 
           :colors => ["Black", "White"], 
-          players => [{}]}, 
+          :players => [{}]}, 
         :away => 
           {:team_name => "Charlotte Hornets", 
           :colors => ["Turquoise", "Purple"], 
-          players => [{}]} }
+          :players => [
+            {:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => }
+            ]
+          } 
+        }
 end
 
 
